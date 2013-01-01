f27359da74fc1626985cc8b2ee58e15f0fbc7a3e
Scott Brady <sbradymobile@gmail.com>
2013-01-01 16:23:25 -0500

    Add volume quick settings tile
    Change-Id: I9cba23c4a6f98a1e1669e363212b9c4875d5ce65
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -60,6 +60,7 @@ public class QuickSettingsUtil {
     public static final String TILE_WIMAX = "toggleWimax";
     public static final String TILE_PROFILE = "toggleProfile";
     public static final String TILE_NFC = "toggleNfc";
+    public static final String TILE_VOLUME = "toggleVolume";
 
     private static final String TILE_DELIMITER = "|";
     protected static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -145,6 +146,9 @@ public class QuickSettingsUtil {
         TILES.put(TILE_USER, new QuickSettingsUtil.TileInfo(
                 TILE_USER, R.string.title_tile_user,
                 "com.android.systemui:drawable/ic_qs_default_user"));
+        TILES.put(TILE_VOLUME, new QuickSettingsUtil.TileInfo(
+                TILE_VOLUME, R.string.title_tile_volume,
+                "com.android.systemui:drawable/ic_qs_ring_on"));
 
 // These toggles are not available yet.  Comment out for now
 //        if(PhoneConstants.LTE_ON_CDMA_TRUE == TelephonyManager.getDefault().getLteOnCdmaMode() ||
