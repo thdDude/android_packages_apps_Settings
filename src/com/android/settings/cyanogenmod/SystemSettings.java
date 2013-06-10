98ecad8fae79dd937f9b4e01e149d2470e1fba7a
drcmda <drcmda@gmail.com>
2013-06-10 09:12:58 +0100

    HALO (2/2)
    HALO is Androids one and only multitasking solution.
    
    We explicitly do not go the Samsung/Cornerstone route. This solution is basically an extension of
    the regular notificationshade, which is an aggregator for notifications that can be checked out when
    time permits. HALO completes it by allowing you to respond to tasks on the spot, trying to be as
    unintrusive as possible and flexible in regards to policies (white/black lists).
    
    Features:
    
    - Apps open up in floating state, apps underneath do not pause
    - complete SystemUI & Core integration, making HALO feel like a native feature, source/SDK and UI/UX
    - HALO has white and blacklists, which for now allow fine control for apps pinging through HALO
    - multiple gestures to navigate HALO:
      - double-tap-move: move
      - tap-drag-left/right: task through notifications, finger up launches
      - tap-drag-up: dismiss notification
      - tap-drag-down: put HALO into semi-hidden state
      - swipe while HALO is hidden wakes it up
      - drag-to-X to remove HALO altogether
    - comes with as few settings as possible, everything should be self explanatory and intutive,
      users must be informed about the double-tap gestures which is the only catch.
    
    Known issues:
    
    - Integration has been finished for PhoneUI. TabletUI and PIE must be integrated still.
    - Packageinstaller must allow HALO through its overlay check or it will disable the install-app button.
    - The low level has a few remaining issues:
      - Some apps still go up fullscale (they trick us with services, circumventing the intent flagging)
      - Some 3D games still pause
      - There is one known app that refuses to end on back or touch-outside, that is WeChat
      - The activitystack pause implementation works but is makeshift
    
    To-Do:
    
    - On-screen tutorial at first start up to teach users the gestures
    - Fixing all known issues, full integration in the remaining UI's
    - Perhaps hybrid engine integration for running floating apps in a decreased size
    
    Conflicts:
    	res/layout/installed_app_details.xml
    	res/values/cm_arrays.xml
    	res/values/cm_strings.xml
    	res/xml/system_settings.xml
    	src/com/android/settings/applications/InstalledAppDetails.java
    	src/com/android/settings/cyanogenmod/SystemSettings.java
    
    Change-Id: I2368b987c41f8c6f66c78bd347314210ea6c19b8

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.cyanogenmod;
 
+import android.app.INotificationManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
@@ -49,13 +50,21 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
     private static final String KEY_POWER_MENU = "power_menu";
     private static final String KEY_EXPANDED_DESKTOP = "expanded_desktop";
     private static final String KEY_EXPANDED_DESKTOP_NO_NAVBAR = "expanded_desktop_no_navbar";
+    private static final String KEY_HALO_STATE = "halo_state";
+    private static final String KEY_HALO_HIDE = "halo_hide";
+    private static final String KEY_HALO_REVERSED = "halo_reversed"; 
 
     private PreferenceScreen mNotificationPulse;
     private PreferenceScreen mBatteryPulse;
     private ListPreference mExpandedDesktopPref;
     private CheckBoxPreference mExpandedDesktopNoNavbarPref;
+    private ListPreference mHaloState;
+    private CheckBoxPreference mHaloHide;
+    private CheckBoxPreference mHaloReversed; 
 
     private boolean mIsPrimary;
+
+    private INotificationManager mNotificationManager; 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +72,9 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
 
         addPreferencesFromResource(R.xml.system_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mNotificationManager = INotificationManager.Stub.asInterface(
+                ServiceManager.getService(Context.NOTIFICATION_SERVICE));
 
             // Only show the hardware keys config on a device that does not have a navbar
             // and the navigation bar config on phones that has a navigation bar
@@ -76,7 +88,7 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
             } catch (RemoteException e) {
                 // Do nothing
 			}
-				
+
         // Determine which user is logged in
         mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
         if (mIsPrimary) {
@@ -90,7 +102,6 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
                     mBatteryPulse = null;
                 }
             }
-
 
             // Act on the above
             if (removeKeys) {
@@ -141,7 +152,29 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
 
         // Don't display the lock clock preference if its not installed
         removePreferenceIfPackageNotInstalled(findPreference(KEY_LOCK_CLOCK));
+
+        mHaloState = (ListPreference) findPreference(KEY_HALO_STATE);
+        mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
+        mHaloState.setOnPreferenceChangeListener(this);
+
+        mHaloHide = (CheckBoxPreference) findPreference(KEY_HALO_HIDE);
+        mHaloHide.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.HALO_HIDE, 0) == 1);
+
+        mHaloReversed = (CheckBoxPreference) findPreference(KEY_HALO_REVERSED);
+        mHaloReversed.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.HALO_REVERSED, 1) == 1); 
+
     }
+
+    private boolean isHaloPolicyBlack() {
+        try {
+            return mNotificationManager.isHaloPolicyBlack();
+        } catch (android.os.RemoteException ex) {
+                // System dead
+        }
+        return true;
+    } 
 
     @Override
     public void onResume() {
@@ -172,10 +205,32 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
             boolean value = (Boolean) objValue;
             updateExpandedDesktop(value ? 2 : 0);
             return true;
+        } else if (preference == mHaloState) {
+            boolean state = Integer.valueOf((String) objValue) == 1;
+            try {
+                mNotificationManager.setHaloPolicyBlack(state);
+            } catch (android.os.RemoteException ex) {
+                // System dead
+            }          
+            return true; 
         }
 
         return false;
     }
+
+     @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if  (preference == mHaloHide) {  
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);  
+        } else if (preference == mHaloReversed) {  
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
+                    ? 1 : 0);  
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    } 
 
     private void updateLightPulseDescription() {
         if (Settings.System.getInt(getActivity().getContentResolver(),
