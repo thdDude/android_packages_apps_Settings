9c7252d2555789b8a611dc07be764b875a728f84
DerTeufel <dominik-kassel@gmx.de>
2013-09-04 13:40:05 +0200

    Revert "Halo: Add master switch to disable Halo (2/2)"
    This reverts commit fa70ec7e6aaccd3dd22d202ba47a21f8de2d0cbc.

--- src/com/android/settings/cyanogenmod/HaloSettings.java
+++ src/com/android/settings/cyanogenmod/HaloSettings.java
@@ -43,7 +43,6 @@ import com.android.settings.Utils;
 public class HaloSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_HALO_ENABLED = "halo_enabled";
     private static final String KEY_HALO_ACTIVE = "halo_active";
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
@@ -54,7 +53,6 @@ public class HaloSettings extends SettingsPreferenceFragment
     private static final String KEY_HALO_PING_COLOR = "halo_ping_color";
     private static final String KEY_HALO_GONE = "halo_gone";
 
-    private CheckBoxPreference mHaloEnabled;
     private CheckBoxPreference mHaloActive;
     private ListPreference mHaloState;
     private CheckBoxPreference mHaloHide;
@@ -78,10 +76,6 @@ public class HaloSettings extends SettingsPreferenceFragment
 
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
-
-        mHaloEnabled = (CheckBoxPreference) findPreference(KEY_HALO_ENABLED);
-        mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_ENABLED, 0) == 1);
 
         mHaloActive = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_ACTIVE);
         mHaloActive.setChecked(Settings.System.getInt(mContext.getContentResolver(),
@@ -127,11 +121,7 @@ public class HaloSettings extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if  (preference == mHaloEnabled) {  
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
-                    ? 1 : 0);  
-        } else if  (preference == mHaloHide) {  
+        if (preference == mHaloHide) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_HIDE, mHaloHide.isChecked()
                     ? 1 : 0);
