71d60d0588b1a8c3e8a8bcfaf43b6a5f7cc5967d
DerTeufel <dominik-kassel@gmx.de>
2013-09-04 13:56:30 +0200

    Revert "Halo settings matching frameworks changes"
    This reverts commit 4a0fc0dd942dafcc23cee5d4b34e10fc8e4eca53.

--- src/com/android/settings/cyanogenmod/HaloSettings.java
+++ src/com/android/settings/cyanogenmod/HaloSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 ParanoidAndroid Project
+ * Copyright (C) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,85 +16,81 @@
 
 package com.android.settings.cyanogenmod;
 
-import android.app.ActivityManager;
-import android.app.AlertDialog;
 import android.app.INotificationManager;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
-import android.view.IWindowManager;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-public class HaloSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
-
-    private static final String KEY_HALO_ACTIVE = "halo_active";
+    public class HaloSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+    private static final String KEY_HALO_ENABLED = "halo_enabled";
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
-    private static final String KEY_HALO_REVERSED = "halo_reversed";
-    private static final String KEY_HALO_PAUSE = "halo_pause";
+    private static final String KEY_HALO_REVERSED = "halo_reversed"; 
+    private static final String KEY_WE_WANT_POPUPS = "show_popup";
     private static final String KEY_HALO_BUTTON_COLOR = "halo_button_color";
     private static final String KEY_HALO_TEXT_BUBBLE_COLOR = "halo_text_bubble_color";
     private static final String KEY_HALO_PING_COLOR = "halo_ping_color";
-
-    private CheckBoxPreference mHaloActive;
+	
+    private CheckBoxPreference mHaloEnabled;
     private ListPreference mHaloState;
     private CheckBoxPreference mHaloHide;
-    private CheckBoxPreference mHaloReversed;
-    private CheckBoxPreference mHaloPause;
+    private CheckBoxPreference mHaloReversed; 
+    private CheckBoxPreference mWeWantPopups;
     private Preference mHaloButtonColor;
     private Preference mHaloTextBubbleColor;
     private Preference mHaloPingColor;
 
-    private Context mContext;
-    private INotificationManager mNotificationManager;
+    private INotificationManager mNotificationManager; 
+    private ContentResolver mContentResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.halo);
+
         PreferenceScreen prefSet = getPreferenceScreen();
-        mContext = getActivity();
+        mContentResolver = getActivity().getApplicationContext().getContentResolver();
 
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
 
-        mHaloActive = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_ACTIVE);
-        mHaloActive.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_ACTIVE, 0) == 1);
+        mHaloEnabled = (CheckBoxPreference) findPreference(KEY_HALO_ENABLED);
+        mHaloEnabled.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.HALO_ENABLED, 0) == 1);
 
-        mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
+        mHaloState = (ListPreference) findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
         mHaloState.setOnPreferenceChangeListener(this);
 
-        mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
-        mHaloHide.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+        mHaloHide = (CheckBoxPreference) findPreference(KEY_HALO_HIDE);
+        mHaloHide.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.HALO_HIDE, 0) == 1);
 
-        mHaloReversed = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_REVERSED);
-        mHaloReversed.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_REVERSED, 1) == 1);
+        mHaloReversed = (CheckBoxPreference) findPreference(KEY_HALO_REVERSED);
+        mHaloReversed.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.HALO_REVERSED, 1) == 1); 
 
-        int isLowRAM = (ActivityManager.isLargeRAM()) ? 0 : 1;
-        mHaloPause = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_PAUSE);
-        mHaloPause.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_PAUSE, isLowRAM) == 1);
+        int showPopups = Settings.System.getInt(getContentResolver(), Settings.System.WE_WANT_POPUPS, 1);
+
+        mWeWantPopups = (CheckBoxPreference) findPreference(KEY_WE_WANT_POPUPS);
+        mWeWantPopups.setOnPreferenceChangeListener(this);
+        mWeWantPopups.setChecked(showPopups > 0);
 
         mHaloButtonColor =
                 (Preference) prefSet.findPreference(KEY_HALO_BUTTON_COLOR);
@@ -113,24 +109,41 @@ public class HaloSettings extends SettingsPreferenceFragment
         return true;
     }
 
-    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mHaloState) {
+            boolean state = Integer.valueOf((String) objValue) == 1;
+            try {
+                mNotificationManager.setHaloPolicyBlack(state);
+            } catch (android.os.RemoteException ex) {
+                // System dead
+            }          
+            return true; 
+        } else if (preference == mWeWantPopups) {
+            boolean checked = (Boolean) objValue;
+                        Settings.System.putInt(getActivity().getContentResolver(),
+                                Settings.System.WE_WANT_POPUPS, checked ? 1 : 0);
+            return true;
+        }
+
+        return false;
+    }
+
+
+     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mHaloHide) {
-            Settings.System.putInt(mContext.getContentResolver(),
+        if  (preference == mHaloEnabled) {  
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
+                    ? 1 : 0);  
+        } else if  (preference == mHaloHide) {  
+            Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.HALO_HIDE, mHaloHide.isChecked()
-                    ? 1 : 0);
-        } else if (preference == mHaloActive) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_ACTIVE, mHaloActive.isChecked()
-                    ? 1 : 0);
-        } else if (preference == mHaloReversed) {
-            Settings.System.putInt(mContext.getContentResolver(),
+                    ? 1 : 0);  
+
+        } else if (preference == mHaloReversed) {  
+            Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
-                    ? 1 : 0);
-        } else if (preference == mHaloPause) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_PAUSE, mHaloPause.isChecked()
-                    ? 1 : 0);
+                    ? 1 : 0);  
         } else if (preference == mHaloButtonColor) {
             ColorPickerDialog cp = new ColorPickerDialog(getActivity(),
                     mButtonColorListener, Settings.System.getInt(mContext.getContentResolver(),
@@ -154,19 +167,6 @@ public class HaloSettings extends SettingsPreferenceFragment
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mHaloState) {
-            boolean state = Integer.valueOf((String) newValue) == 1;
-            try {
-                mNotificationManager.setHaloPolicyBlack(state);
-            } catch (android.os.RemoteException ex) {
-                // System dead
-            }
-            return true;
-        }
-        return false;
     }
 
     ColorPickerDialog.OnColorChangedListener mButtonColorListener =
