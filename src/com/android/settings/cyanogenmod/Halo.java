0a68d4f3db14a32cdd66132c41c931803d8da901
DerTeufel <dominik-kassel@gmx.de>
2013-09-05 18:12:23 +0200

    halo: add missing halo active options
    Change-Id: Ie9c5d872a5c011bf78bd82baf6c94089aa10a058

--- src/com/android/settings/cyanogenmod/Halo.java
+++ src/com/android/settings/cyanogenmod/Halo.java
@@ -35,6 +35,7 @@ public class Halo extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_HALO_ENABLED = "halo_enabled";
+    private static final String KEY_HALO_ACTIVE = "halo_active";
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
@@ -45,6 +46,7 @@ public class Halo extends SettingsPreferenceFragment
     private static final String KEY_HALO_GONE = "halo_gone";
 
     private CheckBoxPreference mHaloEnabled;
+    private CheckBoxPreference mHaloActive;
     private ListPreference mHaloState;
     private ListPreference mHaloSize;
     private CheckBoxPreference mHaloHide;
@@ -71,6 +73,10 @@ public class Halo extends SettingsPreferenceFragment
         mHaloEnabled = (CheckBoxPreference) findPreference(KEY_HALO_ENABLED);
         mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ENABLED, 0) == 1);
+
+        mHaloActive = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_ACTIVE);
+        mHaloActive.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_ACTIVE, 0) == 1);
 
         mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
@@ -127,6 +133,10 @@ public class Halo extends SettingsPreferenceFragment
         } else if  (preference == mHaloHide) {  
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloActive) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_ACTIVE, mHaloActive.isChecked()
                     ? 1 : 0);
         } else if (preference == mHaloReversed) {
             Settings.System.putInt(mContext.getContentResolver(),
